16e8efc24e0c97e9a165d60b915894d6a38d78d0
fellowEngineer <bsuman1921@gmail.com>
2022-12-04 22:32:01 +0530

    Blog post module challange is complete

--- Blog_site/app.js
+++ Blog_site/app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 const { application } = require("express");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -67,6 +68,25 @@ app.post("/compose", (req, res) => {
 })
 
 
+app.get("/posts/:tata", (req, res) => {
+  const currReq = _.lowerCase(req.params.tata);
+
+  Blogs.forEach((element) => {
+    let tmp = _.lowerCase(element.title);
+
+    if(currReq === tmp)
+    {
+      res.render("pages/post", {post: element});
+    }
+  })
+})
+
+
+
+
+
+
+
 
 
 const port = 4000;
@@ -75,7 +95,3 @@ const port = 4000;
 app.listen(process.env.PORT || port, function() {
   console.log("Server started on port: " + port);
 });
-
-
-
-
