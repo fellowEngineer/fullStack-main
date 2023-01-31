1f7356a8bc5c80ebafb34249cc13057ab8dc85c5
fellow_engineer <71657493+fellowEngineer@users.noreply.github.com>
2023-01-31 09:06:10 +0530

    update config.js

--- firebase_learn/firebase_crud/apis/config.js
+++ firebase_learn/firebase_crud/apis/config.js
@@ -1,13 +1,6 @@
 import { initializeApp, firestore } from "firebase";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBgu-nQjStxa7rXzvyLn24K9wlfCaswzqg",
-    authDomain: "test-tube-56dbd.firebaseapp.com",
-    projectId: "test-tube-56dbd",
-    storageBucket: "test-tube-56dbd.appspot.com",
-    messagingSenderId: "578538983190",
-    appId: "1:578538983190:web:6d33d047821cbdd01f0a37"
-};
+const firebaseConfig = {};
 
 initializeApp(firebaseConfig);
 const db = firestore();
